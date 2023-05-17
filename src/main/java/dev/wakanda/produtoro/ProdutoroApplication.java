2c8e6e3399aeef550981e3dbbf4f10297d2911dc
Tamires Rigoti <tamiresrigoti@gmail.com>
2023-05-17 07:05:25 -0300

    init commit

---
+++ src/main/java/dev/wakanda/produtoro/ProdutoroApplication.java
@@ -0,0 +1,13 @@
+package dev.wakanda.produtoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wakanda/produtoro/ProdutoroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakanda.produtoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
